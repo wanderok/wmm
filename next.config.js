04e7248a94b82cd12df11fa210e74d9e9b08586d
wanderok <51582259+wanderok@users.noreply.github.com>
2022-11-30 01:32:36 -0300

    next 10.0.1

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
-/*
 module.exports = {
     webpack: (config, { isServer }) => {
       // Fixes npm packages that depend on `fs` module
       if (!isServer) {
         config.node = {
-          fs: 'empty'
+          fs: 'empty',
+          net: 'empty'
         }
       }
   
       return config
     }
   }
-*/
