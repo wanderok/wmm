d527ce67ccddd6a1453831b5d4a9deb2f57cb210
wanderok <51582259+wanderok@users.noreply.github.com>
2022-11-30 00:53:14 -0300

    excluded next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/*
 module.exports = {
     webpack: (config, { isServer }) => {
       // Fixes npm packages that depend on `fs` module
@@ -10,3 +11,4 @@ module.exports = {
       return config
     }
   }
+*/
