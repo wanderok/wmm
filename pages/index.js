998e81731224d3331b95e1036f21eadd25ece632
wanderok <51582259+wanderok@users.noreply.github.com>
2022-11-30 01:34:32 -0300

    next 13.0.5

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,4 @@
-import myexpress from '../lib/myexpress';
-
-export async function getStaticProps(){
-    const express = require('express');
-    return {
-        message,
-    }
-}
-
-export default function Home({message}){
-//function Home(){
-    return <div>PIPOCA!</div>
+function Home(){
+    return <div>PIPOCA! again...</div>
 }
 
