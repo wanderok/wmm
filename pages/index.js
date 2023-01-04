c8c6f59794e08b7ef4b44c130f0805ac5eebff57
wanderok <51582259+wanderok@users.noreply.github.com>
2023-01-04 15:50:28 -0300

    const definitions

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-//const express = require("express")
-//const app = express()
-//const port = 8081
+const express = require("express")
+const app = express()
+const port = 8081
 
 //app.get("/", function (req, res) {
 //    res.send("Seja bem vindo.")
