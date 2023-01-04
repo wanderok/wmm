15ae393d5eecfd63a8eb2caed07acc57589110ec
wanderok <51582259+wanderok@users.noreply.github.com>
2023-01-04 13:09:35 -0300

    como antes

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
-const express = require("express")
-const app = express()
-const port = 8081
+//const express = require("express")
+//const app = express()
+//const port = 8081
 
-app.get("/", function (req, res) {
-    res.send("Seja bem vindo.")
-});
+//app.get("/", function (req, res) {
+//    res.send("Seja bem vindo.")
+//});
 
 // this is the last line of the code
-app.listen(port, function () {
-    console.log("servidor running...")
-});
+//app.listen(port, function () {
+    //console.log("servidor running...")
+//});
 
 
-//function Home() {
-//    return <div>PIPOCA!</div>
-//}
-//export default Home
+function Home() {
+    return <div>PIPOCA!</div>
+}
+
+export default Home
